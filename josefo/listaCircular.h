303e25b24365aaee22f8d6ab236b5f3dd7b9a044
Mateus <mateus.ferro.2001@hotmail.com>
2021-10-05 11:49:24 -0300

    header file listaCircular.h

--- josefo/listaCircular.h
+++ josefo/listaCircular.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef LISTA_CIRCULAR_H
+#define LISTA_CIRCULAR_H
+
+#include <string>
+
+using TipoDado = char;        //typedef char TipoDado;
+
+struct No {
+    TipoDado info;
+    No* prox;
+};
+
+class Lista {
+public:
+    Lista();
+    ~Lista();
+    void insere(TipoDado x);
+    TipoDado remove();
+    unsigned int tam() const;
+    bool vazia() const;
+    void imprime() const;
+
+protected:
+    No* lista;
+    unsigned int numnos;
+};
+
+#endif
