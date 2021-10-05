3d74a457fbe2266cbb405490b2f603f5fc7d7006
Mateus <mateus.ferro.2001@hotmail.com>
2021-10-05 11:51:09 -0300

    Add constructors and functions to listaCircular.cpp

---
+++ josefo/listaCircular.cpp
@@ -0,0 +1,56 @@
+#include "listaCircular.h"
+#include <iostream>
+
+using namespace std;
+
+Lista::Lista() {
+	lista = nullptr;
+	numnos = 0;
+}
+
+Lista::~Lista() {
+	cout << "Deletando memoria da lista" << endl;
+	while (!vazia()) { cout << remove() << endl; }
+}
+
+void Lista::insere(TipoDado x) {
+	No* novono = new No;
+	novono->info = x;
+
+	if (!vazia()) {
+		novono->prox = lista->prox;
+		lista->prox = novono;
+	}
+	else novono->prox = novono;
+
+	lista = novono;
+	++numnos;
+}
+
+TipoDado Lista::remove() {
+	TipoDado aux;
+	if (!vazia()) {
+		No* rem = lista->prox;
+		lista->prox = rem->prox;
+		aux = rem->info;
+		delete rem;
+		--numnos;
+		if (numnos == 0) lista = nullptr;
+	}
+	else cerr << "lista vazia!" << endl;
+}
+
+unsigned int Lista::tam() const { return numnos; }
+
+bool Lista::vazia() const { return (numnos == 0); }
+
+void Lista::imprime() const {
+	No* tmp = lista->prox;
+	if (!vazia()) {
+		while (tmp != lista) {
+			cout << tmp->info << ", ";
+			tmp = tmp->prox;
+		}
+	}
+	cout << tmp->info << endl;
+}
